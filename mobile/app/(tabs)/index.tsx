c240cf85ed9c26cadeab034095c5b9a223ea31c6
lukewarm3 <lcao3@nd.edu>
2025-03-30 21:56:20 -0400

    add the logo

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -40,7 +40,24 @@ export default function IndexScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Social Cooking</Text>
+      <View
+        style={{
+          flex: 0.4,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 20,
+          alignSelf: "flex-start",
+          marginLeft: 25,
+          gap: 10,
+        }}
+      >
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={{ width: 40, height: 40 }}
+        />
+        <Text style={styles.title}>Social Cooking</Text>
+      </View>
       <View style={{ height: "75%", width: "90%", marginVertical: 10 }}>
         <RecipeCard
           image={spicyTomatoImage}
@@ -78,9 +95,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "#D98324",
-    marginTop: 20,
-    alignSelf: "flex-start",
-    marginLeft: 25,
   },
   description: {
     fontSize: 16,
